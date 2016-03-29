43e765e1533bfefefaa7c4b53e8d2dc207be5fff
aldiilhamabubakar <aldibakar67@gmail.com>
2016-03-29 09:44:28 +0700

    Added files via upload

---
+++ Fgjfgj.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fgjfgj;
+import java.util.Scanner;
+/**
+ *
+ * @author Praktikum
+ */
+public class Fgjfgj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] bil = new int [100];
+        bil[0] = 1;
+        bil[1] = 1;
+        int deret;
+        
+        Scanner input = new Scanner (System.in);
+        System.out.println("Masukan jumlah deret");
+        String data = input.nextLine();
+        
+        deret= Integer.parseInt(data);
+        
+        for (int i = 0; i < deret; i++) {
+            System.out.println(bil[i]+" ");
+            bil[i + 2] = bil [i] + bil[i + 1];
+            
+            
+        }
+    }
+    
+}
