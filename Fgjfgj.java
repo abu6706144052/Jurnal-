5cc44d9a3d7abe4e330ea8d7f5371ca1ebfa8831
aldiilhamabubakar <aldibakar67@gmail.com>
2016-03-29 09:56:26 +0700

    Update Fgjfgj.java

--- Fgjfgj.java
+++ Fgjfgj.java
@@ -20,6 +20,7 @@ public class Fgjfgj {
         bil[0] = 1;
         bil[1] = 1;
         int deret;
+        int jumlah = 0;
         
         Scanner input = new Scanner (System.in);
         System.out.println("Masukan jumlah deret");
@@ -28,8 +29,9 @@ public class Fgjfgj {
         deret= Integer.parseInt(data);
         
         for (int i = 0; i < deret; i++) {
-            System.out.println(bil[i]+" ");
+            System.out.println(jumlah+" ");
             bil[i + 2] = bil [i] + bil[i + 1];
+            jumlah += bil[i];
             
             
         }
